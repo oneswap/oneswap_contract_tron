5782ca7aeb3323c36d5e2cac2660eeffb598976c
oneswapdev <developer@oneswap.net>
2020-11-12 17:10:17 +0800

    OneSwap's smartcontract source code for tron

---
+++ OneSwapFactory.sol
@@ -0,0 +1,110 @@
+// SPDX-License-Identifier: GPL
+pragma solidity 0.6.12;
+
+import "./interfaces/IOneSwapFactory.sol";
+import "./OneSwapPair.sol";
+
+contract OneSwapFactory is IOneSwapFactory {
+    struct TokensInPair {
+        address stock;
+        address money;
+    }
+
+    address public override feeTo;
+    address public override feeToSetter;
+    address public immutable gov;
+    address public immutable ones;
+    uint32 public override feeBPS = 50;
+    address public override pairLogic;
+    mapping(address => TokensInPair) private _pairWithToken;
+    mapping(bytes32 => address) private _tokensToPair;
+    address[] public allPairs;
+
+    constructor(address _feeToSetter, address _gov, address _ones, address _pairLogic) public {
+        feeToSetter = _feeToSetter;
+        gov = _gov;
+        ones = _ones;
+        pairLogic = _pairLogic;
+    }
+
+    function createPair(address stock, address money, bool isOnlySwap) external override returns (address pair) {
+        require(stock != money, "OneSwapFactory: IDENTICAL_ADDRESSES");
+        // not necessary //require(stock != address(0) || money != address(0), "OneSwapFactory: ZERO_ADDRESS");
+        uint moneyDec = _getDecimals(money);
+        uint stockDec = _getDecimals(stock);
+        require(23 >= stockDec && stockDec >= 0, "OneSwapFactory: STOCK_DECIMALS_NOT_SUPPORTED");
+        uint dec = 0;
+        if (stockDec >= 4) {
+            dec = stockDec - 4; // now 19 >= dec && dec >= 0
+        }
+        // 10**19 = 10000000000000000000
+        //  1<<64 = 18446744073709551616
+        uint64 priceMul = 1;
+        uint64 priceDiv = 1;
+        bool differenceTooLarge = false;
+        if (moneyDec > stockDec) {
+            if (moneyDec > stockDec + 19) {
+                differenceTooLarge = true;
+            } else {
+                priceMul = uint64(uint(10)**(moneyDec - stockDec));
+            }
+        }
+        if (stockDec > moneyDec) {
+            if (stockDec > moneyDec + 19) {
+                differenceTooLarge = true;
+            } else {
+                priceDiv = uint64(uint(10)**(stockDec - moneyDec));
+            }
+        }
+        require(!differenceTooLarge, "OneSwapFactory: DECIMALS_DIFF_TOO_LARGE");
+        bytes32 salt = keccak256(abi.encodePacked(stock, money, isOnlySwap));
+        require(_tokensToPair[salt] == address(0), "OneSwapFactory: PAIR_EXISTS");
+        OneSwapPairProxy oneswap = new OneSwapPairProxy{salt: salt}(stock, money, isOnlySwap, uint64(uint(10)**dec), priceMul, priceDiv, ones);
+
+        pair = address(oneswap);
+        allPairs.push(pair);
+        _tokensToPair[salt] = pair;
+        _pairWithToken[pair] = TokensInPair(stock, money);
+        emit PairCreated(pair, stock, money, isOnlySwap);
+    }
+
+    function _getDecimals(address token) private view returns (uint) {
+        if (token == address(0)) { return 6; }
+        return uint(IERC20(token).decimals());
+    }
+
+    function allPairsLength() external override view returns (uint) {
+        return allPairs.length;
+    }
+
+    function setFeeTo(address _feeTo) external override {
+        require(msg.sender == feeToSetter, "OneSwapFactory: FORBIDDEN");
+        feeTo = _feeTo;
+    }
+
+    function setFeeToSetter(address _feeToSetter) external override {
+        require(msg.sender == feeToSetter, "OneSwapFactory: FORBIDDEN");
+        feeToSetter = _feeToSetter;
+    }
+
+    function setPairLogic(address implLogic) external override {
+        require(msg.sender == gov, "OneSwapFactory: SETTER_MISMATCH");
+        pairLogic = implLogic;
+    }
+
+    function setFeeBPS(uint32 _bps) external override {
+        require(msg.sender == gov, "OneSwapFactory: SETTER_MISMATCH");
+        require(0 <= _bps && _bps <= 50 , "OneSwapFactory: BPS_OUT_OF_RANGE");
+        feeBPS = _bps;
+    }
+
+    function getTokensFromPair(address pair) external view override returns (address stock, address money) {
+        stock = _pairWithToken[pair].stock;
+        money = _pairWithToken[pair].money;
+    }
+
+    function tokensToPair(address stock, address money, bool isOnlySwap) external view override returns (address pair) {
+        bytes32 key = keccak256(abi.encodePacked(stock, money, isOnlySwap));
+        return _tokensToPair[key];
+    }
+}
---
+++ OneSwapRouter.sol
@@ -0,0 +1,160 @@
+// SPDX-License-Identifier: GPL
+pragma solidity 0.6.12;
+
+import "./interfaces/IOneSwapRouter.sol";
+import "./interfaces/IOneSwapFactory.sol";
+import "./interfaces/IOneSwapPair.sol";
+import "./interfaces/IERC20.sol";
+import "./libraries/SafeMath256.sol";
+import "./libraries/DecFloat32.sol";
+
+
+contract OneSwapRouter is IOneSwapRouter {
+    using SafeMath256 for uint;
+    address public immutable override factory;
+
+    modifier ensure(uint deadline) {
+        // solhint-disable-next-line not-rely-on-time,
+        require(deadline >= block.timestamp, "OneSwapRouter: EXPIRED");
+        _;
+    }
+
+    constructor(address _factory) public {
+        factory = _factory;
+    }
+
+    //fallback() external payable {}
+
+    function _addLiquidity(address pair, uint amountStockDesired, uint amountMoneyDesired,
+        uint amountStockMin, uint amountMoneyMin) private view returns (uint amountStock, uint amountMoney) {
+
+        (uint reserveStock, uint reserveMoney, ) = IOneSwapPool(pair).getReserves();
+        if (reserveStock == 0 && reserveMoney == 0) {
+            (amountStock, amountMoney) = (amountStockDesired, amountMoneyDesired);
+        } else {
+            uint amountMoneyOptimal = _quote(amountStockDesired, reserveStock, reserveMoney);
+            if (amountMoneyOptimal <= amountMoneyDesired) {
+                require(amountMoneyOptimal >= amountMoneyMin, "OneSwapRouter: INSUFFICIENT_MONEY_AMOUNT");
+                (amountStock, amountMoney) = (amountStockDesired, amountMoneyOptimal);
+            } else {
+                uint amountStockOptimal = _quote(amountMoneyDesired, reserveMoney, reserveStock);
+                assert(amountStockOptimal <= amountStockDesired);
+                require(amountStockOptimal >= amountStockMin, "OneSwapRouter: INSUFFICIENT_STOCK_AMOUNT");
+                (amountStock, amountMoney) = (amountStockOptimal, amountMoneyDesired);
+            }
+        }
+    }
+
+    function addLiquidity(address stock, address money, bool isOnlySwap, uint amountStockDesired,
+        uint amountMoneyDesired, uint amountStockMin, uint amountMoneyMin, address to, uint deadline) external
+        payable override ensure(deadline) returns (uint amountStock, uint amountMoney, uint liquidity) {
+
+        if (stock != address(0) && money != address(0)) {
+            require(msg.value == 0, 'OneSwapRouter: NOT_ENTER_ETH_VALUE');
+        }
+        address pair = IOneSwapFactory(factory).tokensToPair(stock, money, isOnlySwap);
+        if (pair == address(0)) {
+            pair = IOneSwapFactory(factory).createPair(stock, money, isOnlySwap);
+        }
+        (amountStock, amountMoney) = _addLiquidity(pair, amountStockDesired,
+            amountMoneyDesired, amountStockMin, amountMoneyMin);
+        _safeTransferFrom(stock, msg.sender, pair, amountStock);
+        _safeTransferFrom(money, msg.sender, pair, amountMoney);
+        liquidity = IOneSwapPool(pair).mint(to);
+        emit AddLiquidity(amountStock, amountMoney, liquidity);
+    }
+
+    function _removeLiquidity(address pair, uint liquidity, uint amountStockMin,
+        uint amountMoneyMin, address to) private returns (uint amountStock, uint amountMoney) {
+        IERC20(pair).transferFrom(msg.sender, pair, liquidity);
+        (amountStock, amountMoney) = IOneSwapPool(pair).burn(to);
+        require(amountStock >= amountStockMin, "OneSwapRouter: INSUFFICIENT_STOCK_AMOUNT");
+        require(amountMoney >= amountMoneyMin, "OneSwapRouter: INSUFFICIENT_MONEY_AMOUNT");
+    }
+
+    function removeLiquidity(address pair, uint liquidity, uint amountStockMin, uint amountMoneyMin,
+        address to, uint deadline) external override ensure(deadline) returns (uint amountStock, uint amountMoney) {
+        // ensure pair exist
+        _getTokensFromPair(pair);
+        (amountStock, amountMoney) = _removeLiquidity(pair, liquidity, amountStockMin, amountMoneyMin, to);
+    }
+
+    function _swap(address input, uint amountIn, address[] memory path, address _to) internal virtual returns (uint[] memory amounts) {
+        amounts = new uint[](path.length + 1);
+        amounts[0] = amountIn;
+
+        for (uint i = 0; i < path.length; i++) {
+            (address to, bool isLastSwap) = i < path.length - 1 ? (path[i+1], false) : (_to, true);
+            amounts[i + 1] = IOneSwapPair(path[i]).addMarketOrder(input, to, uint112(amounts[i]));
+            if (!isLastSwap) {
+                (address stock, address money) = _getTokensFromPair(path[i]);
+                input = (stock != input) ? stock : money;
+            }
+        }
+    }
+
+    function swapToken(address token, uint amountIn, uint amountOutMin, address[] calldata path,
+        address to, uint deadline) external payable override ensure(deadline) returns (uint[] memory amounts) {
+
+        if (token != address(0)) { require(msg.value == 0, 'OneSwapRouter: NOT_ENTER_ETH_VALUE'); }
+        require(path.length >= 1, "OneSwapRouter: INVALID_PATH");
+        // ensure pair exist
+        _getTokensFromPair(path[0]);
+        _safeTransferFrom(token, msg.sender, path[0], amountIn);
+        amounts = _swap(token, amountIn, path, to);
+        require(amounts[path.length] >= amountOutMin, "OneSwapRouter: INSUFFICIENT_OUTPUT_AMOUNT");
+    }
+
+    function limitOrder(bool isBuy, address pair, uint prevKey, uint price, uint32 id,
+        uint stockAmount, uint deadline) external payable override ensure(deadline) {
+
+        (address stock, address money) = _getTokensFromPair(pair);
+        {
+            (uint _stockAmount, uint _moneyAmount) = IOneSwapPair(pair).calcStockAndMoney(uint64(stockAmount), uint32(price));
+            if (isBuy) {
+                if (money != address(0)) { require(msg.value == 0, 'OneSwapRouter: NOT_ENTER_ETH_VALUE'); }
+                _safeTransferFrom(money, msg.sender, pair, _moneyAmount);
+            } else {
+                if (stock != address(0)) { require(msg.value == 0, 'OneSwapRouter: NOT_ENTER_ETH_VALUE'); }
+                _safeTransferFrom(stock, msg.sender, pair, _stockAmount);
+            }
+        }
+        IOneSwapPair(pair).addLimitOrder(isBuy, msg.sender, uint64(stockAmount), uint32(price), id, uint72(prevKey));
+    }
+
+    // todo. add encoded bytes interface for limitOrder.
+
+    function _safeTransferFrom(address token, address from, address to, uint value) internal {
+        if (token == address(0)) {
+            _safeTransferETH(to, value);
+            uint inputValue = msg.value;
+            if (inputValue > value) { _safeTransferETH(msg.sender, inputValue - value); }
+            return;
+        }
+
+        uint beforeAmount = IERC20(token).balanceOf(to);
+        // bytes4(keccak256(bytes('transferFrom(address,address,uint256)')));
+        // solhint-disable-next-line avoid-low-level-calls
+        (bool success, bytes memory data) = token.call(abi.encodeWithSelector(0x23b872dd, from, to, value));
+        require(success && (data.length == 0 || abi.decode(data, (bool))), "OneSwapRouter: TRANSFER_FROM_FAILED");
+        uint afterAmount = IERC20(token).balanceOf(to);
+        require(afterAmount == beforeAmount + value, "OneSwapRouter: TRANSFER_FAILED");
+    }
+
+    function _safeTransferETH(address to, uint value) internal {
+        // solhint-disable-next-line avoid-low-level-calls
+        (bool success,) = to.call{value:value}(new bytes(0));
+        require(success, "TransferHelper: ETH_TRANSFER_FAILED");
+    }
+
+    function _quote(uint amountA, uint reserveA, uint reserveB) internal pure returns (uint amountB) {
+        require(amountA > 0, "OneSwapRouter: INSUFFICIENT_AMOUNT");
+        require(reserveA > 0 && reserveB > 0, "OneSwapRouter: INSUFFICIENT_LIQUIDITY");
+        amountB = amountA.mul(reserveB) / reserveA;
+    }
+
+    function _getTokensFromPair(address pair) internal view returns(address stock, address money) {
+        (stock, money) = IOneSwapFactory(factory).getTokensFromPair(pair);
+        require(stock != address(0) || money != address(0), "OneSwapRouter: PAIR_NOT_EXIST");
+    }
+}
